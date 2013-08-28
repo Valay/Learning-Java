10036384205114c80d24a33a314e396e20620a12
Valay Shah <valayshah@cs.brown.edu>
2013-08-28 15:03:39 -0400

    MaximumSubarray problem now works

--- divideANDconquer/MaximumSubarray.java
+++ divideANDconquer/MaximumSubarray.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.List;
-import java.util.ArrayList;
 import java.io.IOException;
 
 public class MaximumSubarray{
@@ -9,7 +7,7 @@ public class MaximumSubarray{
 		Integer[] ret_values = new Integer[3];
 		try{
 			
-		
+			// Test example : 100 113 110 85 105 102 86 63 81 101 94 106 101 79 94 90 97
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			String[] StockData;
 			StockData = (br.readLine()).split(" ");
@@ -29,6 +27,10 @@ public class MaximumSubarray{
 			ret_values = maximumSubarray(difference,1,difference.length-1);
 		
 			System.out.println("The max sum is "+ret_values[0]+ " with start "+ret_values[1]+" and end "+ret_values[2]);
+			
+			for(int j=0;j<difference.length;j++){
+				System.out.println(difference[j]);
+			}
 		}catch(IOException e){
 			e.printStackTrace();
 		}
@@ -49,7 +51,7 @@ public class MaximumSubarray{
 		Integer[] RightArray;
 		Integer[] CrossArray;
 
-		mid = (int) Math.ceil(( (double)end - (double)start)/2.0);
+		mid = (int) Math.floor(( (double)end + (double)start)/2.0);
 
 		LeftArray = maximumSubarray(diff,0,mid);
 		
@@ -81,7 +83,7 @@ public class MaximumSubarray{
 				max_left = i;
 			}
 		}
-		
+		sum = 0;
 		// Find max-sum on right side
 		for(int j=mid+1; j<=end; j++){
 			sum = sum + diff[j];
