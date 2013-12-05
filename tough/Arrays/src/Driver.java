f18affb7aff35352bbc3f462ef1d3d85ff2c58ca
Valay <valayshah26@gmail.com>
2013-12-04 22:16:35 -0500

    Majority Element

--- tough/Arrays/src/Driver.java
+++ tough/Arrays/src/Driver.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Driver {
@@ -17,14 +19,27 @@ public class Driver {
 	 */
 	
 	public static void main(String[] args){
-		String[] s;
+		String[] numbers = null;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		try {
-			s = br.readLine().split(" ");
+			numbers = br.readLine().split(" ");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		List<Integer> nums = new ArrayList<Integer>();
+		for(String s:numbers){
+			nums.add(Integer.parseInt(s));
+		}
+		
+		/*
+		 * Majority Test
+		 * 5 4 6 7 5 3 5 5 2 5 5 7 5 8 5 5 1 1
+		 * 3 4 5 9 9 7 8 2 1 2 0 3 4 8 5 4 1
+		 */
+		MajorityElement me = new MajorityElement();
+		System.out.println(me.isMajor(nums));
 		
 		// call functions that uses array
 		
---
+++ tough/Arrays/src/MajorityElement.java
@@ -0,0 +1,47 @@
+import java.util.List;
+
+
+public class MajorityElement {
+
+	public boolean isMajor(List<Integer> nums){
+		
+		int x = MajorityElement.findCandidate(nums);
+		// Make one pass through the list to check if this is indeed a majority element
+		int count = 0;
+		for(int i=0;i<nums.size();i++){
+			if(nums.get(i) == x){
+				count++;
+			}
+		}
+		if( 2*count >= nums.size()){
+			System.out.println(x+" : "+count);
+			
+			return true;
+		}
+		return false;
+	}
+	
+	public static int findCandidate(List<Integer> nums){
+		int n = nums.size();
+		
+		if(n == 1){
+			return nums.get(0);
+		}
+		
+		int major = nums.get(0);
+		int count = 1;
+		for(int i=1;i<n;i++){
+			if(nums.get(i) == major){
+				count++;
+			}else{
+				count--;
+			}
+			
+			if(count == 0){
+				major = nums.get(i);
+				count = 1;
+			}
+		}
+		return major;
+	}
+}
