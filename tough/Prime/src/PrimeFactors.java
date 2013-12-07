b7ba397243acc3de754454e00344dd725bd7783e
Valay <valayshah26@gmail.com>
2013-12-07 00:37:59 -0500

    Print Factors completed

--- tough/Prime/src/PrimeFactors.java
+++ tough/Prime/src/PrimeFactors.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -7,29 +9,30 @@ import java.util.TreeSet;
 public class PrimeFactors {
 
 	public static void main(String[] args){
-			int n = 24;
+			System.out.println(args[0]+" "+args[1]);
+			int n = Integer.parseInt(args[1]);
 			PrimeFactors pf = new PrimeFactors();
 			pf.printFactors(n);
-			
 		}
 	
 	public void printFactors(int n){
 		System.out.println("1 * "+n);
-		Set<TreeSet<Integer>> treeset = fetchFactors(n);
-//		for(TreeSet<Integer> ts : treeset){
-//			for(Integer i : ts){
-//				System.out.print(i+" * ");
-//			}
-//			System.out.println("\r\r\r\n");
-//		}
+		Set<ArrayList<Integer>> treeset = fetchFactors(n);
+		for(ArrayList<Integer> ts : treeset){
+			System.out.print(ts.get(0));
+			for(int i=1;i<ts.size();i++){
+				System.out.print(" * "+ts.get(i));
+			}
+			System.out.print("\n");
+		}
 	}
 	
-	private Set<TreeSet<Integer>> fetchFactors(int n){
-		Set<TreeSet<Integer>> s = new HashSet<TreeSet<Integer>>();
+	private Set<ArrayList<Integer>> fetchFactors(int n){
+		Set<ArrayList<Integer>> s = new HashSet<ArrayList<Integer>>();
 		if(n == 1){
-			TreeSet<Integer> temp = new TreeSet<Integer>();
-			temp.add(1);
+			ArrayList<Integer> temp = new ArrayList<Integer>();
 			s.add(temp);
+			return s;
 		}
 		
 		Set<Integer> factors = this.getFactors(n);
@@ -41,17 +44,22 @@ public class PrimeFactors {
 			cur = it.next();
 			mul = n/cur;
 			//System.out.println(cur+" "+mul);
-			Set<TreeSet<Integer>> sts = fetchFactors(mul);
-			if(sts.isEmpty()){
-				TreeSet<Integer> temp = new TreeSet<Integer>();
-				temp.add(cur);
-				sts.add(temp);
-			}
-			System.out.println(sts);
-			for(TreeSet<Integer> temp : sts){
-				TreeSet<Integer> ts = new TreeSet<Integer>(temp);
+			Set<ArrayList<Integer>> sts = fetchFactors(mul);
+
+			for(ArrayList<Integer> temp : sts){
+				ArrayList<Integer> ts = new ArrayList<Integer>(temp);
 				ts.add(cur);
+				Collections.sort(ts);
+				//System.out.println(ts);
 				s.add(ts);
+			}
+			// also add the cur and mul to the list
+			if(mul != 1){
+				ArrayList<Integer> cm = new ArrayList<Integer>();
+				cm.add(cur);
+				cm.add(mul);
+				Collections.sort(cm);
+				s.add(cm);
 			}
 		}
 		return s;
